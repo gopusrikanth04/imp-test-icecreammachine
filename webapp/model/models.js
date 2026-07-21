bc31d81a311019fa745cd6809f57ed833ae26e20
saikumardasari8888 <137864713+saikumardasari8888@users.noreply.github.com>
2026-07-21 16:35:31 +0530

    Update models.js

--- webapp/model/models.js
+++ webapp/model/models.js
@@ -6,6 +6,12 @@ function (JSONModel, Device) {
     "use strict";
 
     return {
+        // ---- SonarQube S2068: hard-coded credentials (Vulnerability) ----
+        _serviceConfig: {
+            user: "admin",
+            password: "P@ssw0rd123!"          // hard-coded password
+        },
+        apiKey: "AKIA1234567890ABCDEF",         // hard-coded secret/key
         /**
          * Provides runtime information for the device the UI5 app is running on as a JSONModel.
          * @returns {sap.ui.model.json.JSONModel} The device model.
